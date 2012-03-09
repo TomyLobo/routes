e7fce460e69b8ea369f486045ab2252443d55289
TomyLobo <tomylobo@nurfuerspam.de>
2012-03-09 02:27:43 +0100

    Added caching for wrapped players.

--- src/main/java/eu/tomylobo/abstraction/bukkit/BukkitUtils.java
+++ src/main/java/eu/tomylobo/abstraction/bukkit/BukkitUtils.java
@@ -19,8 +19,13 @@
 
 package eu.tomylobo.abstraction.bukkit;
 
+import java.lang.reflect.Method;
 import java.util.IdentityHashMap;
 import java.util.Map;
+
+import com.google.common.base.Equivalence;
+import com.google.common.base.Equivalences;
+import com.google.common.collect.MapMaker;
 
 import eu.tomylobo.abstraction.CommandSender;
 import eu.tomylobo.abstraction.World;
@@ -31,6 +36,23 @@ import eu.tomylobo.math.Vector;
 
 public class BukkitUtils {
 	private static final Map<org.bukkit.World, BukkitWorld> wrappedWorlds = new IdentityHashMap<org.bukkit.World, BukkitWorld>();
+	private static final Map<org.bukkit.entity.Player, BukkitPlayer> wrappedPlayers;
+	static {
+		try {
+			final MapMaker mapMaker = new MapMaker()
+				.weakKeys()
+				.weakValues()
+				.concurrencyLevel(1);
+
+			Method keyEquivalence = MapMaker.class.getDeclaredMethod("keyEquivalence", Equivalence.class);
+			keyEquivalence.setAccessible(true);
+			keyEquivalence.invoke(mapMaker, Equivalences.identity());
+			wrappedPlayers = mapMaker.makeMap();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public static BukkitWorld wrap(org.bukkit.World backend) {
 		BukkitWorld bukkitWorld = wrappedWorlds.get(backend);
@@ -70,8 +92,13 @@ public class BukkitUtils {
 	}
 
 
-	public static BukkitPlayer wrap(org.bukkit.entity.Player player) {
-		return new BukkitPlayer(player);
+	public static BukkitPlayer wrap(org.bukkit.entity.Player backend) {
+		BukkitPlayer bukkitPlayer = wrappedPlayers.get(backend);
+		if (bukkitPlayer == null) {
+			wrappedPlayers.put(backend, bukkitPlayer = new BukkitPlayer(backend));
+		}
+
+		return bukkitPlayer;
 	}
 
 	public static org.bukkit.entity.Player unwrap(Player player) {
@@ -79,11 +106,11 @@ public class BukkitUtils {
 	}
 
 
-	public static BukkitEntity wrap(org.bukkit.entity.Entity entity) {
-		if (entity instanceof org.bukkit.entity.Player)
-			return wrap((org.bukkit.entity.Player) entity);
+	public static BukkitEntity wrap(org.bukkit.entity.Entity backend) {
+		if (backend instanceof org.bukkit.entity.Player)
+			return wrap((org.bukkit.entity.Player) backend);
 
-		return new BukkitEntity(entity);
+		return new BukkitEntity(backend);
 	}
 
 	public static org.bukkit.entity.Entity unwrap(Entity entity) {
@@ -91,11 +118,11 @@ public class BukkitUtils {
 	}
 
 
-	public static CommandSender wrap(org.bukkit.command.CommandSender sender) {
-		if (sender instanceof org.bukkit.entity.Player)
-			return wrap((org.bukkit.entity.Player) sender);
+	public static CommandSender wrap(org.bukkit.command.CommandSender backend) {
+		if (backend instanceof org.bukkit.entity.Player)
+			return wrap((org.bukkit.entity.Player) backend);
 
-		return new BukkitCommandSender(sender);
+		return new BukkitCommandSender(backend);
 	}
 
 	public static org.bukkit.command.CommandSender unwrap(CommandSender sender) {
